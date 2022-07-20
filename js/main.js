fe8e8a0b76acbba12050b18a4320849ef86bf520
KnifeF <knifef@protonmail.com>
2022-07-20 15:01:04 +0300

    Pre-preparation of files and TODOs

---
+++ js/main.js
@@ -0,0 +1,167 @@
+'use strict'
+
+/*
+Functionality and Features
+● Show a timer that starts on first click (right / left) and stops
+when game is over.
+● Left click reveals the cell’s content
+● Right click flags/unflags a suspected cell (you cannot reveal a
+flagged cell)
+● Game ends when:
+o LOSE: when clicking a mine, all mines should be revealed
+o WIN: all the mines are flagged, and all the other cells are
+shown
+● Support 3 levels of the game
+o Beginner (4*4 with 2 MINES)
+o Medium (8 * 8 with 12 MINES)
+o Expert (12 * 12 with 30 MINES)
+● If you have the time, make your Minesweeper look great.
+● Expanding: When left clicking on cells there are 3 possible
+cases we want to address:
+o MINE – reveal the mine clicked
+o Cell with neighbors – reveal the cell alone
+o Cell without neighbors – expand it and its 1st degree
+neighbors
+*/
+
+// TODO-1: the seed app
+// ##########################
+// 1. Create a 4x4 gBoard Matrix containing Objects. 
+// Place 2 mines manually when each cell’s isShown set to true.
+// 2. Present the mines using renderBoard() function.
+// ##########################
+// TODO-2: counting neighbors
+// ##########################
+// 1. Create setMinesNegsCount() and store the numbers
+// (isShown is still true)
+// 2. Present the board with the neighbor count and the mines
+// using renderBoard() function.
+// 3. Have a console.log presenting the board content – to help
+// you with debugging
+// ##########################
+// TODO-3: click to reveal
+// ##########################
+// 1. Make sure your renderBoard() function adds the cell ID to
+// each cell and onclick on each cell calls cellClicked()
+// function.
+// 2. Make the default “isShown” to be “false”
+// 3. Implement that clicking a cell with “number” reveals the
+// number of this cell
+// ##########################
+// TODO-4: randomize mines' location
+// ##########################
+// 1. Randomly locate the 2 mines on the board
+// 2. Present the mines using renderBoard() function.
+
+
+
+const MINE = '🔲'
+const SMILEY = '💎'
+const EMPTY = ' '
+
+var gTimer
+
+var gMinesNegCount
+
+// The model
+// var gBoard = {
+//     minesAroundCount: 4,
+//     isShown: true,
+//     isMine: false,
+//     isMarked: true
+// }
+
+
+// This is an object by which the
+// board size is set (in this case:
+// 4x4 board and how many mines
+// to put)
+// var gLevel = {
+//     SIZE: 4,
+//     MINES: 2
+// }
+
+
+// This is an object in which you
+// can keep and update the
+// current game state:
+// isOn: Boolean, when true we
+// let the user play
+// shownCount: How many cells
+// are shown
+// markedCount: How many cells
+// are marked (with a flag)
+// secsPassed: How many seconds passed 
+// var gGame = {
+//     isOn: false,
+//     shownCount: 0,
+//     markedCount: 0,
+//     secsPassed: 0
+// }
+
+
+// This is called when page loads
+function initGame() {
+    /**
+     * initializes the minesweeper game
+     */
+}
+
+function buildBoard() {
+    /**
+     * Builds the board
+     * Set mines at random locations
+     * Call setMinesNegsCount()
+     * Return the created board
+     */
+}
+
+function setMinesNegsCount(board) {
+    /**
+     * Count mines around each cell
+     * and set the cell's
+     * minesAroundCount
+     */
+}
+
+// function with same name on utils.js!!
+// function renderBoard(board) {
+//     /**
+//      * Render the board as a <table> to the page
+//      */
+// }
+
+
+// Search the web (and
+// implement) how to hide the
+// context menu on right click
+function cellClicked(elCell, i, j) {
+    /**
+     * Called on right click to mark a cell 
+     * (suspected to be a mine)
+     */
+}
+
+function checkGameOver() {
+    /**
+     * Game ends when all mines are marked, 
+     * and all the other cells are shown
+
+     */
+}
+
+// NOTE: start with a basic
+// implementation that only opens
+// the non-mine 1st degree
+// neighbors
+// BONUS: if you have the time
+// later, try to work more like the
+// real algorithm (see description
+// at the Bonuses section below)
+function expandShown(board, elCell, i, j) {
+    /**
+     * When user clicks a cell with no mines around, 
+     * we need to open not only that cell, 
+     * but also its neighbors. 
+     */
+}
